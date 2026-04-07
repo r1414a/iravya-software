fa5483f1d1ec75640009420206a9fb75df941d51
manjusha-0103 <manjushakatkhede96@gmail.com>
2026-04-07 19:08:40 +0530

    APIS-> Add Truc, Update Truck, Delete, Get all Truck with search and filter, added ts_vector

--- server/controller/truckController.js
+++ server/controller/truckController.js
@@ -1,17 +1,180 @@
 import asyncHandler from "../utils/asyncHandler.js"
 import  ApiError  from "../utils/ApiError.js";
 import sendResponse from "../utils/sendResponse.js";
-import { addTruckservice } from "../services/truck.services.js";
+import { addTruckservice , 
+    uploadFile,
+    truckExistByRegNoService,
+    getTruckDataService,
+    updateTruckDataService,
+    truckExistByIDService,
+    deleteTruckService,
+    getRecentTripDetailsService,
+    getTripHistoryService,
+    getAllTruckDataService
+} from "../services/truck.services.js";
 
-const addTruck = asyncHandler(async(req,res)=>{
 
-    const truck = await addTruckservice(req.body)
-    if(truck.length>0){
-        sendResponse(res,200, truck, "Truck data is added successs=fully")
+
+const addTruck = asyncHandler(async (req, res) => {
+
+    const isTruckExist = await truckExistByRegNoService(
+        req.body.registration_no
+    )
+
+    if (isTruckExist) {
+        return sendResponse(
+            res,
+            200,
+            { is_exist: isTruckExist },
+            "Truck already exists"
+        )
     }
-    
+
+    const registration_cert = req.files?.registration_cert
+        ? await uploadFile(req.files.registration_cert[0], "Truck")
+        : null
+
+    const insurance_doc = req.files?.insurance_doc
+        ? await uploadFile(req.files.insurance_doc[0], "Truck")
+        : null
+
+    const PUC_cert = req.files?.PUC_cert
+        ? await uploadFile(req.files.PUC_cert[0], "Truck")
+        : null
+
+    const truck = await addTruckservice({
+        ...req.body,
+        registration_cert,
+        insurance_doc,
+        PUC_cert
+    })
+
+
+    return sendResponse(
+        res,
+        200,
+        truck,
+        "Truck added successfully"
+    )
+
 })
 
+
+const getTrukData = asyncHandler(async (req, res) => {
+    const {id} = req.params
+    const truck = await getTruckDataService(id)
+    // console.log(truck)
+    if(truck){
+        sendResponse(res, 201, truck, "Truck data found")
+    }
+    else{
+        throw new ApiError( 400, "Could not find truck data" )
+    }
+})
+
+const updateTruckData = asyncHandler(async (req, res) => {
+    console.log(req.params)
+    const { id } = req.params
+    console.log(id)
+    const isTruckExist = await truckExistByIDService(id)
+
+    if (!isTruckExist) {
+        return sendResponse(
+            res,
+            400,
+            { is_exist: isTruckExist },
+            "Truck data is not added"
+        )
+    }
+    const registration_cert = req.files?.registration_cert
+        ? await uploadFile(req.files.registration_cert[0], "Truck")
+        : undefined
+
+    const insurance_doc = req.files?.insurance_doc
+        ? await uploadFile(req.files.insurance_doc[0], "Truck")
+        : undefined
+
+    const PUC_cert = req.files?.PUC_cert
+        ? await uploadFile(req.files.PUC_cert[0], "Truck")
+        : undefined
+
+    const truck = await updateTruckDataService(id, {
+        ...req.body,
+        registration_cert,
+        insurance_doc,
+        PUC_cert
+    })
+
+    return sendResponse(
+        res,
+        200,
+        truck,
+        "Truck updated successfully"
+    )
+})
+
+const deleteTruck = asyncHandler(async (req, res) => {
+    const {id} = req.params
+    const truck = await deleteTruckService(id)
+    if(truck){
+        sendResponse(res, 200, truck, "Truck data is deleted successfully")
+    }
+    else{
+        throw new ApiError( 400, "Could not find truck data" )
+    }
+})
+
+const getRecentTripDetails = async(req, res)=>{
+    const{trip_id} = req.body
+    const data = await getRecentTripDetailsService(trip_id)
+    if(data){
+        sendResponse(res, 201, data, "Found Data")
+    }
+    else{
+        throw new ApiError(400, "Data not found")
+    }
+}
+
+const getTripHistory = asyncHandler(async(req, res)=>{
+    const {id} = req.params
+    const trips = await getTripHistoryService(id)
+
+    if(trips){
+        sendResponse(res, 201, trips, "Found Data")
+    }
+    else{
+        throw ApiError(200, "Not Found Data")
+    }
+})
+
+const getAllTruckData = asyncHandler(async (req, res) => {
+    const { type, status, search, page = 1, 
+        limit = 10  } = req.query
+
+    const trucks = await getAllTruckDataService({
+        type: type || null,
+        truck_status : status || null,
+        search : search || null,
+        page: Number(page),
+        limit: Number(limit)
+    })
+
+    return sendResponse(
+        res,
+        200,
+        trucks,
+        "Truck list fetched successfully"
+    )
+})
+ 
+
+
 export{
-    addTruck
+    addTruck,
+    getTrukData,
+    updateTruckData,
+    deleteTruck,
+    getRecentTripDetails,
+    getTripHistory,
+    getAllTruckData
 }
--- server/routes/truckRoutes.js
+++ server/routes/truckRoutes.js
@@ -3,10 +3,44 @@ import { protect } from "../middleware/authmiddleware.js";
 import { authorize } from "../middleware/authoriseRoleMiddleware.js";
 import validate from "../middleware/validate.js";
 import express from 'express'
+import { addTruck,
+    getTrukData,
+    updateTruckData,
+    deleteTruck,
+    getRecentTripDetails,
+    getTripHistory,
+    getAllTruckData
+ } from "../controller/truckController.js";
+import {addTruckValidation,
+    updateTruckValidation
+} from "../validations/truck.validations.js"
 
 
 const router = express.Router()
 
+router.post(
+  "/add-truck",
+  protect,
+  authorize('super_admin',"dc_manager"),
+  upload.fields([
+    { name: "registration_cert" },
+    { name: "insurance_doc" },
+    { name: "PUC_cert" }
+  ]),
+  addTruckValidation,
+  validate,
+  addTruck
+)
 
+router.get("/truck/:id", protect, authorize('super_admin',"dc_manager"),getTrukData)
+router.put("/updatetruck/:id", protect, authorize('super_admin',"dc_manager"), upload.fields([
+    { name: "registration_cert" },
+    { name: "insurance_doc" },
+    { name: "PUC_cert" }
+  ]), updateTruckData, validate, updateTruckData)
 
+router.delete("/truck/delete/:id", protect, authorize('super_admin',"dc_manager"),deleteTruck)
+router.get("/trip-data", protect, authorize('super_admin',"dc_manager"), getRecentTripDetails)
+router.get('/trip-history/:id', protect, authorize('super_admin',"dc_manager"),getTripHistory)
+router.get("/truck", protect, authorize('super_admin',"dc_manager"), getAllTruckData)
 export default router

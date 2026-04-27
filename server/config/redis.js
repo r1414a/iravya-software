496b076e8b18e7d9055ce0d53709773dffd32218
r1414a <rupeshchincholkar14@gmail.com>
2026-04-27 13:33:55 +0530

    firebase configuration server

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,12 +1,14 @@
 import * as redis from 'redis';
 
-const client = redis.createClient({
-  url: process.env.REDIS_URL
-});
+// const client = redis.createClient({
+//   url: process.env.REDIS_URL
+// });
 
-client.on('error', (err) => console.error('Redis Client Error', err));
+// client.on('error', (err) => console.error('Redis Client Error', err));
 
-await client.connect();
-console.log('Connected to Redis');
+// await client.connect();
+// console.log('Connected to Redis');
+
+const client = null
 
 export default client;
